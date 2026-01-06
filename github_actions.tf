1980714240352f59c6f335ccc5025f183d8f5eed
Andy Price <sixdaysandy@gmail.com>
2026-01-06 12:20:04 +0000

    SP-3367 - Allow Github Actions to be restricted to specific actions #minor

--- github_actions.tf
+++ github_actions.tf
@@ -5,3 +5,14 @@ resource "github_actions_secret" "repository_secret" {
   secret_name     = each.key
   plaintext_value = each.value
 }
+
+resource "github_actions_repository_permissions" "repository" {
+  count           = length(var.allowed_github_actions) > 0 ? 1 : 0
+  allowed_actions = "selected"
+  allowed_actions_config {
+    github_owned_allowed = true
+    patterns_allowed     = var.allowed_github_actions
+    verified_allowed     = true
+  }
+  repository = github_repository.repository.name
+}
